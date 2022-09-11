96213abbd95525cba69993683761dc8f3d57fa13
sahupratik30 <sahupratik30@gmail.com>
2022-09-11 20:34:43 +0530

    initial

---
+++ day-3.js
@@ -0,0 +1,11 @@
+const str = "JavaScript is awesome";
+
+function reverseAString(str) {
+  let revStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str.charAt(i);
+  }
+  return revStr;
+}
+
+console.log(`Reversed string is: ${reverseAString(str)}`);
