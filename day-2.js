395ad7b84b18f159428a8877c64d6560948bbe8a
sahupratik30 <sahupratik30@gmail.com>
2022-09-11 20:19:40 +0530

    initial

---
+++ day-2.js
@@ -0,0 +1,14 @@
+// Write a program to reverse a string
+
+const str = "JavaScript is awesome";
+
+function reverseAString(str) {
+  // write your solution here
+  let revStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str.charAt(i);
+  }
+  return revStr;
+}
+
+console.log(`Reversed string is: ${reverseAString(str)}`);
