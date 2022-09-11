4df1fcdd8fbc2b5131bc9aae9fcd68ebef81ff1f
sahupratik30 <sahupratik30@gmail.com>
2022-09-11 20:32:34 +0530

    initial

--- day-2.js
+++ day-2.js
@@ -3,7 +3,6 @@
 const str = "JavaScript is awesome";
 
 function reverseAString(str) {
-  // write your solution here
   let revStr = "";
   for (let i = str.length - 1; i >= 0; i--) {
     revStr += str.charAt(i);
